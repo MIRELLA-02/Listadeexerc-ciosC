1389f7ef15339f988997be093a4f52fc9435a3d1
MIRELLA-02 <brolezimirella@gmail.com>
2025-05-13 08:51:48 -0300

    Add files via upload

---
+++ EXERCÍCIO 6- LISTA C.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main (){
+     int contador, i;
+    
+     for(i = 1; i<=100; i++){
+         printf ("%d\n", i);
+         
+     }
+
+     
+     return 0;
+    
+     }
+     //Utilizado o for como estrutura de repetição, sempre que o número for maior que 1 e menor ou igual a 100, o código vai somar e imprimir na tela de 1 a 100 os números.
+     
+     
