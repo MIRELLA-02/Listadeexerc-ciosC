1dfd0152bd1672e07c7b14a0259a53cd92d6cca7
MIRELLA-02 <brolezimirella@gmail.com>
2025-05-13 10:33:38 -0300

    Add files via upload

---
+++ EXERCÍCIO 10- LISTA C.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    int num, i, res = 1;
+    
+    printf("Digite um número:\n");
+    scanf("%d", &num);
+    
+    for (i = 1; i <= num; i++) {
+        res = res * i;
+}
+      printf("O fatorial deste número é: %d\n", res);
+      
+      return 0;
+}
